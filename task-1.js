9434883ea12fb4d71647def4410d130ada28941a
Sttacy <139151264+Sttacy@users.noreply.github.com>
2023-07-16 16:46:12 +0300

    Add files via upload

---
+++ task-1.js
@@ -0,0 +1,8 @@
+const name = "Генератор";
+let price = 1000;
+const result = `Обрано ${name}, ціна за одиницю ${price} кредитів `;
+console.log(result);
+
+price = 2000;
+const result2 = `Обрано ${name}, ціна за одиницю ${price} кредитів `;
+console.log(result2);
---
+++ task-2.js
@@ -0,0 +1,5 @@
+const total = 100
+const ordered = 50
+
+const message = total < ordered ? "На складі недостатня кількість товару!" : "Замовлення оформлено";
+console.log(message);
